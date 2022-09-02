930f17b004b945f9e935c275253565451c5841ff
dbrll <dbrll@users.noreply.github.com>
2022-09-02 19:35:08 +0000

    GPL license header

--- src/avgpwr.c
+++ src/avgpwr.c
@@ -1,4 +1,22 @@
-//gcc ./avgpwr.c -o avgpwr -O2  -lrtlsdr -Wall
+/*
+* Reads samples from an RTLSDR dongle at a given frequency and sample rate,
+* then computes their averaged power over a period of time.
+*
+* Copyright (C) 2022 Damien Boureille
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
--- src/proc.c
+++ src/proc.c
@@ -1,4 +1,23 @@
-// gcc proc.c -lm -O2 -o proc -Wall
+/*
+* Reads the frequency/power values from a spectum and a reference file. 
+* Then performs the reference division, RFI removal, slope correction, 
+* conversion to radial velocity, and outputs the resulting values.
+*
+* Copyright (C) 2022 Damien Boureille
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
